5103e2018d87b5459886d1879d940188646d7968
antoine <antoine.doeraene@b12-consulting.com>
2022-08-13 23:36:01 +0200

    using only one canvas for all colour evaluation

--- web-components/src/main/scala/be/doeraene/webcomponents/ui5/scaladsl/colour/FromStringColourCache.scala
+++ web-components/src/main/scala/be/doeraene/webcomponents/ui5/scaladsl/colour/FromStringColourCache.scala
@@ -23,11 +23,12 @@ trait FromStringColourCache {
 object FromStringColourCache {
 
   val noCache: FromStringColourCache = new FromStringColourCache {
+    val canvas = dom.document.createElement("canvas").asInstanceOf[dom.HTMLCanvasElement]
+    canvas.width = 1
+    canvas.height = 1
+    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+
     def fromString(colourString: String): Colour = {
-      val canvas = dom.document.createElement("canvas").asInstanceOf[dom.HTMLCanvasElement]
-      canvas.width = 1
-      canvas.height = 1
-      val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
       ctx.fillStyle = colourString
       ctx.fillRect(0, 0, 1, 1)
       val data  = ctx.getImageData(0, 0, 1, 1).data
