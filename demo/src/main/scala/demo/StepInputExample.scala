ce851ba0be9e0fd9238f2afa8cafcf0d1c81dada
antoine <antoine.doeraene@b12-consulting.com>
2022-08-14 22:11:56 +0200

    StepInput component and example

--- demo/src/main/scala/demo/StepInputExample.scala
+++ demo/src/main/scala/demo/StepInputExample.scala
@@ -9,6 +9,53 @@ object StepInputExample extends Example("StepInput") {
 
   def component(using
       demoPanelInfoMap: FetchDemoPanelFromGithub.CompleteDemoPanelInfo
-  ): HtmlElement = missing
+  ): HtmlElement = div(
+    //-- Begin Common
+    styleTag("""
+    |.shorter {
+    |  width: 300px;
+    |}
+    |""".stripMargin)
+    //-- End Common
+    ,
+    DemoPanel("Basic Step Input") {
+      //-- Begin: Basic Step Input
+      div(
+        className := "shorter",
+        StepInput(_.value := 5),
+        StepInput(_.readonly := true, _.value := 5),
+        StepInput(_.disabled := true, _.value := 5)
+      )
+      //-- End
+    },
+    DemoPanel("Step Input with alignment") {
+      //-- Begin: Step Input with alignment
+      div(
+        className := "shorter",
+        StepInput(_.value := 5),
+        StepInput(_.value := 5, _ => textAlign := "center"),
+        StepInput(_.value := 5, _ => textAlign := "right")
+      )
+      //-- End
+    },
+    DemoPanel("Step Input with min, max, step and valuePrecision") {
+      //-- Begin: Step Input with min, max, step and valuePrecision
+      div(
+        className := "shorter",
+        StepInput(_.value := 5, _.min := 0, _.max := 10, _.step := 1),
+        StepInput(_.value := 0, _.min := -100, _.max := 100, _.step := 10),
+        StepInput(_.value := 10, _.min := 0, _.max := 20, _.valuePrecision := 1)
+      )
+      //-- End
+    },
+    DemoPanel("Step Input with Value State") {
+      //-- Begin: Step Input with Value State
+      div(
+        className := "shorter",
+        ValueState.allValues.map(state => StepInput(_.valueState := state, _ => marginTop := "0.5rem"))
+      )
+      //-- End
+    }
+  )
 
 }
---
+++ web-components/src/main/scala/be/doeraene/webcomponents/ui5/StepInput.scala
@@ -0,0 +1,81 @@
+package be.doeraene.webcomponents.ui5
+
+import be.doeraene.webcomponents.ui5.configkeys.{ButtonDesign, ColourScheme, IconName}
+import be.doeraene.webcomponents.ui5.internal.Slot
+import com.raquo.domtypes.generic.codecs.{BooleanAsAttrPresenceCodec, StringAsIsCodec}
+import com.raquo.laminar.api.L.*
+import com.raquo.laminar.builders.HtmlTag
+import com.raquo.laminar.keys.{ReactiveHtmlAttr, ReactiveProp, ReactiveStyle}
+import com.raquo.laminar.nodes.ReactiveHtmlElement
+import org.scalajs.dom
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+import com.raquo.domtypes.generic.codecs.DoubleAsStringCodec
+import com.raquo.domtypes.generic.codecs.IntAsStringCodec
+import be.doeraene.webcomponents.ui5.configkeys.ValueState
+import be.doeraene.webcomponents.ui5.eventtypes.EventWithPreciseTarget
+
+/** The ui5-step-input consists of an input field and buttons with icons to increase/decrease the value with the
+  * predefined step.
+  *
+  * The user can change the value of the component by pressing the increase/decrease buttons, by typing a number
+  * directly, by using the keyboard up/down and page up/down, or by using the mouse scroll wheel. Decimal values are
+  * supported.
+  *
+  * @see
+  *   <a href="https://sap.github.io/ui5-webcomponents/playground/components/StepInput/">the doc</a> for more
+  *   information.
+  */
+object StepInput extends HasAccessibleName with HasName {
+
+  @js.native
+  trait RawElement extends js.Object {
+    def value: Double = js.native
+  }
+
+  @js.native
+  @JSImport("@ui5/webcomponents/dist/StepInput.js", JSImport.Default)
+  object RawImport extends js.Object
+
+  // object-s are lazy so you need to actually use them in your code to prevent dead code elimination
+  used(RawImport)
+
+  type Ref         = dom.html.Element with RawElement
+  type ModFunction = StepInput.type => Mod[ReactiveHtmlElement[Ref]]
+
+  private val tag: HtmlTag[Ref] = customHtmlTag("ui5-step-input")
+
+  val id: ReactiveProp[String, String] = idAttr
+
+  val disabled: ReactiveHtmlAttr[Boolean] = customHtmlAttr("disabled", BooleanAsAttrPresenceCodec)
+
+  val max: ReactiveHtmlAttr[Double] = customHtmlAttr("max", DoubleAsStringCodec)
+
+  val min: ReactiveHtmlAttr[Double] = customHtmlAttr("min", DoubleAsStringCodec)
+
+  val placeholder: ReactiveHtmlAttr[String] = customHtmlAttr("placeholder", StringAsIsCodec)
+
+  val readonly: ReactiveHtmlAttr[Boolean] = customHtmlAttr("readonly", BooleanAsAttrPresenceCodec)
+
+  val required: ReactiveHtmlAttr[Boolean] = customHtmlAttr("required", BooleanAsAttrPresenceCodec)
+
+  val step: ReactiveHtmlAttr[Double] = customHtmlAttr("step", DoubleAsStringCodec)
+
+  val value: ReactiveHtmlAttr[Double] = customHtmlAttr("value", DoubleAsStringCodec)
+
+  val valuePrecision: ReactiveHtmlAttr[Int] = customHtmlAttr("value-precision", IntAsStringCodec)
+
+  val valueState: ReactiveHtmlAttr[ValueState] = customHtmlAttr("value-state", ValueState.AsStringCodec)
+
+  object slots {
+    val valueStateMessage: Slot = Slot("valueStateMessage")
+  }
+
+  object events {
+    val onChange: EventProp[EventWithPreciseTarget[Ref]] = new EventProp("change")
+  }
+
+  def apply(mods: ModFunction*): HtmlElement = tag(mods.map(_(StepInput)): _*)
+
+}
