56f2e5c4f0ba8bc002a783a23415de75829402a2
Sven Behrens <s.behrens@usu.de>
2022-10-16 16:22:32 +0200

    Improve layout with another "overflowY: auto" to avoid scrolling the navigation with the main content

--- demo/src/main/scala/demo/EntryPoint.scala
+++ demo/src/main/scala/demo/EntryPoint.scala
@@ -9,7 +9,6 @@ import be.doeraene.webcomponents.ui5.configkeys.LinkTarget
 
 object EntryPoint {
   def main(args: Array[String]): Unit = {
-    ResponsivePopover
     val componentsDemo: List[Example] = List(
       AvatarExample,
       AvatarGroupExample,
@@ -80,21 +79,31 @@ object EntryPoint {
 
     val componentName = Option(new URL(dom.document.location.href).searchParams.get("componentName")).getOrElse("")
 
-
     render(
       dom.document.getElementById("root"),
       if componentName == "" then
         div(
           padding := "1em",
           h1("Demo of SAP UI5 bindings for Laminar"),
-          p("This is the demo page for the ", 
-            Link(_.target := LinkTarget._blank, _.href := "https://sap.github.io/ui5-webcomponents/", "SAP UI5"), " bindings for ", 
-            Link(_.target := LinkTarget._blank, _.href := "https://laminar.dev/", "Laminar"), "."
+          p(
+            "This is the demo page for the ",
+            Link(_.target := LinkTarget._blank, _.href := "https://sap.github.io/ui5-webcomponents/", "SAP UI5"),
+            " bindings for ",
+            Link(_.target := LinkTarget._blank, _.href := "https://laminar.dev/", "Laminar"),
+            "."
           ),
-          p("The library repo is available ", Link(_.target := LinkTarget._blank, _.href := "https://github.com/sherpal/LaminarSAPUI5Bindings", "here"), "."),
+          p(
+            "The library repo is available ",
+            Link(_.target := LinkTarget._blank, _.href := "https://github.com/sherpal/LaminarSAPUI5Bindings", "here"),
+            "."
+          ),
           h2("Choose one of the components below:"),
           ul(
-            componentsDemo.map(_.name).map(componentName => li(Link(componentName, _.href := dom.document.location.pathname ++ s"?componentName=$componentName")))
+            componentsDemo
+              .map(_.name)
+              .map(componentName =>
+                li(Link(componentName, _.href := dom.document.location.pathname ++ s"?componentName=$componentName"))
+              )
           )
         )
       else
@@ -104,25 +113,33 @@ object EntryPoint {
             width := "300px",
             SideNavigation(
               _.events.onSelectionChange.map(_.detail.item.dataset.get("componentName")) --> Observer[Option[String]] {
-                case Some(componentName) => dom.document.location.href = dom.document.location.pathname ++ s"?componentName=$componentName"
+                case Some(componentName) =>
+                  dom.document.location.href = dom.document.location.pathname ++ s"?componentName=$componentName"
                 case None => throw new IllegalArgumentException(s"This item did not have data 'componentName'.")
               },
               componentsDemo.map(example =>
                 SideNavigation.item(
                   _.text := example.name,
-                  width := "200px",
+                  width                      := "200px",
                   dataAttr("component-name") := example.name,
                   _.selected := (example.name == componentName)
                 )
               ),
-              height := "100vh",
+              height    := "100vh",
               overflowY := "auto"
             )
           ),
           div(
-            padding := "10px",
-            maxWidth := "calc(100% - 320px)",
-            componentsDemo.find(_.name == componentName).map(_.completeComponent).getOrElse(div("Not Found"))
+            height    := "100vh",
+            overflowY := "auto",
+            display   := "flex",
+            flexGrow  := 1,
+            div(
+              padding  := "10px",
+              minWidth := "40%",
+              maxWidth := "calc(100% - 320px)",
+              componentsDemo.find(_.name == componentName).map(_.completeComponent).getOrElse(div("Not Found"))
+            )
           )
         )
     )
