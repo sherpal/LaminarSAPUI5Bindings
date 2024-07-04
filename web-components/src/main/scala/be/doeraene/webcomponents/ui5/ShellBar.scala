625bbf8227c31979e5ae922a35b698c6569187fa
Simon Parten <Quafadas@users.noreply.github.com>
2024-07-04 14:04:39 +0200

    Update ShellBar.scala

--- web-components/src/main/scala/be/doeraene/webcomponents/ui5/ShellBar.scala
+++ web-components/src/main/scala/be/doeraene/webcomponents/ui5/ShellBar.scala
@@ -40,7 +40,7 @@ object ShellBar extends WebComponent with HasIcon {
   }
 
   @js.native
-  @JSImport("@ui5/webcomponents-fiori/dist/ShellBar", JSImport.Default)
+  @JSImport("@ui5/webcomponents-fiori/dist/ShellBar.js", JSImport.Default)
   object RawImport extends js.Object
 
   // object-s are lazy so you need to actually use them in your code to prevent dead code elimination
