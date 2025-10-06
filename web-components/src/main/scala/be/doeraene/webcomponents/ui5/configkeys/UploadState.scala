91e4d8b06e9a89699bde43f5708d849b2f56eda5
Ivan Klass <klass.ivanklass@gmail.com>
2025-10-06 12:26:58 +0200

    Add UploadState.Complete

--- web-components/src/main/scala/be/doeraene/webcomponents/ui5/configkeys/UploadState.scala
+++ web-components/src/main/scala/be/doeraene/webcomponents/ui5/configkeys/UploadState.scala
@@ -5,11 +5,12 @@ sealed trait UploadState {
 }
 
 object UploadState extends EnumerationString[UploadState] {
+  case object Complete extends UploadState
   case object Ready extends UploadState
   case object Uploading extends UploadState
   case object Error extends UploadState
 
-  val allValues: List[UploadState] = List(Ready, Uploading, Error)
+  val allValues: List[UploadState] = List(Complete, Ready, Uploading, Error)
 
   def valueOf(value: UploadState): String = value.value
 }
